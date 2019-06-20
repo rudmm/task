c259413b4a0951b994aba0f1013ad5a0d556f1de
Mykhailo Rud <rudmm1996@gmail.com>
2019-06-20 15:13:14 +0300

    20.06 15.13

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-   var scroll = $(window).scrollTop();
+   let scroll = $(window).scrollTop();
 
 
    if (scroll >= 1) {
@@ -8,3 +8,27 @@ $(window).scroll(function() {
        $('.menu').removeClass('fixed');
    }
 });
+$('.videoh').addClass('translate');
+let video1 = document.querySelector('.video1').getBoundingClientRect();
+$(window).scroll(function(){
+  let scroll = $(window).scrollTop();
+  if(scroll>=511){
+    $('.videoh').removeClass('translate');
+    
+  }else{
+    $('.videoh').addClass('translate');
+  }
+});
+
+let img2 = document.querySelector('.part2 .img2').getBoundingClientRect();
+console.log(img2.top, img2.bottom, img2.left, img2.right);
+$(window).scroll(function(){
+  let scroll = $(window).scrollTop();
+  console.log(scroll);
+  if(scroll>=1089 && scroll<=1756){
+    $('.quote').addClass('translate');
+  }else{
+    $('.quote').removeClass('translate');
+  }
+});
+$('.quote').addClass('translate');
